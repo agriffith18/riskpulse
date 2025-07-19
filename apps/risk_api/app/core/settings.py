53b49c5f557190d5028a2738f6472e5add29abde
Arthur Griffith <arthur.griffith94@gmail.com>
2025-07-19 16:52:52 -0400

    chore: update .env variables directory path

--- apps/risk_api/app/core/settings.py
+++ apps/risk_api/app/core/settings.py
@@ -1,15 +1,17 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings
 from pydantic import ConfigDict
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 class Settings(BaseSettings):
     MONGO_URL: str 
     SECRET: str 
     ALGORITHM: str
     # can add more, e.g. JWT_SECRET: str, REDIS_URL: str, etc.
 
-    model_config = ConfigDict(
-        env_file = "../../../.env",
-        env_file_encoding = "utf-8",
-    )
+    model_config = ConfigDict()
 
 # create a single settings instance
 settings = Settings()
